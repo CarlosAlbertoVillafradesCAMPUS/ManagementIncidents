42496fa1acd04b0a5d454c42a4aaa0f372cb53ba
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-09-21 08:37:22 -0500

    feat: :sparkles: Creation of endpoint 3
    The third endpoint was created to obtain specific type incidents reported by a camper
    
    by:Carlos Villafrades

--- api/versions/V1/routes/report_incidents.js
+++ api/versions/V1/routes/report_incidents.js
@@ -39,4 +39,42 @@ appReportIncidents.get("/", async(req,res)=>{
     }
 })
 
+//Listar todos los incidentes materiales reportados por un campista
+//http://127.17.0.96:5099/incidents/Digital?nit=1006654874
+appReportIncidents.get("/:tipo", async(req,res)=>{
+    try {
+        const {tipo} = req.params
+        const {nit} = req.query; 
+        const collection = dataBase.collection("Users")
+        const data = await collection.aggregate([
+            {
+                $lookup: {
+                  from: "Report_Incidents",
+                  localField: "Nit",
+                  foreignField: "By_Camper.Nit",
+                  as: "Incidents_Report"
+                }
+            },
+            {
+                $match:{
+                    Nit:parseInt(nit),
+                    "Incidents_Report.Incident_Type": tipo
+                }
+            },
+            {
+                $project: {
+                    _id:0,
+                    Password:0,
+                    "Incidents_Report._id":0
+                }
+               
+            }
+        ]).toArray()
+        res.status(200).send({status:200, data:data})
+    } catch (error) {
+        res.status(400).send({status:200, message:"Data retrieval error"})
+    }
+})
+
+
 export default appReportIncidents

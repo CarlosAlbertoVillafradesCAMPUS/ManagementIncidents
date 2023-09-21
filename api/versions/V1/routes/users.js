ac8ed87e1d4e287625b9dcd1ebf82d8bc477a32f
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-09-21 07:28:48 -0500

    feat: :sparkles: Creation of the endpoint 1
    The first endpoint was created to fetch campers
    
    by:Carlos Villafrades

--- api/versions/V1/routes/users.js
+++ api/versions/V1/routes/users.js
@@ -4,13 +4,31 @@ import { myConnect } from "../../../db/connect.js";
 const appUser = Router();
 const dataBase = await myConnect();
 
-appUser.get("/trainers", async(req,res)=>{
+//Listar todos los Trainers
+//http://127.17.0.96:5099/users?rol=Admin
+appUser.get("/", async(req,res)=>{
     try {
+        const {rol} = req.query; 
+        if(rol === "Admin"){
+            res.status(400).send({status:200, message:"Data retrieval error"})
+        }
         const collection = dataBase.collection("Users")
-        const data = await collection.find().toArray()
-        res.send(data)
+        const data = await collection.aggregate([
+            {
+                $match:{
+                    Role: rol
+                }
+                
+            },
+            {
+                $project: {
+                  _id:0
+                }
+            }
+        ]).toArray()
+        res.status(200).send({status:200, data:data})
     } catch (error) {
-        
+        res.status(400).send({status:200, message:"Data retrieval error"})
     }
 })
 

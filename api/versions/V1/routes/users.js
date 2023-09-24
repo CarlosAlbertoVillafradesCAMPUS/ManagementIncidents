c0149a76ffa23881e0d2a11e5c2b8fabd29421dc
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-09-23 20:36:37 -0500

    feat: :sparkles: Creating endpoints 19,20 and 21
    These endpoints first list all users, then list users by role, and finally search for all users in general.
    
    by: Carlos Villafrades

--- api/versions/V1/routes/users.js
+++ api/versions/V1/routes/users.js
@@ -8,27 +8,74 @@ const dataBase = await myConnect();
 //http://127.17.0.96:5099/users?rol=Admin
 appUser.get("/", async(req,res)=>{
     try {
-        const {rol} = req.query; 
-        if(rol === "Admin"){
-            res.status(400).send({status:200, message:"Data retrieval error"})
+        if (req.query.rol) {
+            const {rol} = req.query; 
+            if(rol === "Admin"){
+               return res.status(400).send({status:200, message:"Data retrieval error"})
+            }
+            const collection = dataBase.collection("Users")
+            const data = await collection.aggregate([
+                {
+                    $match:{
+                        Role: rol
+                    }
+                    
+                },
+                {
+                    $project: {
+                      _id:0,
+                      Password:0
+                    }
+                }
+            ]).toArray()
+            return res.status(200).send({status:200, data:data})
         }
         const collection = dataBase.collection("Users")
         const data = await collection.aggregate([
             {
-                $match:{
-                    Role: rol
-                }
-                
-            },
-            {
                 $project: {
-                  _id:0
+                  _id:0,
+                  Password:0
                 }
             }
         ]).toArray()
         res.status(200).send({status:200, data:data})
+       
     } catch (error) {
-        res.status(400).send({status:200, message:"Data retrieval error"})
+        res.status(400).send({status:400, message:"Data retrieval error"})
+    }
+})
+
+//Crear un buscador para los usuarios
+//http://127.17.0.96:5099/users/SearchGeneral?text=""
+appUser.get("/SearchGeneral", async(req,res)=>{
+    try {
+        const {text} = req.query;
+        const collection = dataBase.collection("Users")
+        const data = await collection.aggregate([
+            {
+                $match: {
+                    Role: {
+                        $not: {
+                            $eq: "Admin",
+                          },
+                      },
+                Nickname: {
+                    $regex: new RegExp('^' +text, 'i'),
+                  },
+                },
+            },
+            {
+                $project: {
+                  _id:0,
+                  Password:0
+                }
+            }
+        ]).toArray()
+        res.status(200).send({status:200, data:data})
+       
+    } catch (error) {
+        res.status(400).send({status:400, message:"Data retrieval error"})
     }
 })
 

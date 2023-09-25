1a88021fd97a59f45c71d4c4b270495978ed49dd
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-09-24 19:32:28 -0500

    feat: :sparkles: Creation of endpoint 26
    The 26th endpoint was created to return a list of all areas.
    
    By: Carlos Villafrades

--- api/versions/V1/index.js
+++ api/versions/V1/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import appUser from "./routes/users.js";
 import appReportIncidents from "./routes/report_incidents.js";
+import appZones from "./routes/zones.js";
 
 const RoutesV1 = Router();
 
 RoutesV1.use("/users", appUser);
 RoutesV1.use("/incidents", appReportIncidents);
+RoutesV1.use("/zones", appZones);
 
 export default RoutesV1
--- api/versions/V1/routes/zones.js
+++ api/versions/V1/routes/zones.js
@@ -1,43 +1,26 @@
 import { Router } from "express";
 import { myConnect } from "../../../db/connect.js";
 
-const appUser = Router();
+const appZones = Router();
 const dataBase = await myConnect();
 
-//Listar todos los Trainers, campers o support
-//http://127.17.0.96:5099/users?rol=Admin
-appUser.get("/", async(req,res)=>{
+//Listar todos las zonas
+//http://127.17.0.96:5099/zones?Area
+appZones.get("/Area", async(req,res)=>{
     try {
-        if (req.query.rol) {
-            const {rol} = req.query; 
-            if(rol === "Admin"){
-               return res.status(400).send({status:200, message:"Data retrieval error"})
-            }
-            const collection = dataBase.collection("Users")
-            const data = await collection.aggregate([
-                {
-                    $match:{
-                        Role: rol
-                    }
-                    
-                },
-                {
-                    $project: {
-                      _id:0,
-                      Password:0
-                    }
-                }
-            ]).toArray()
-            return res.status(200).send({status:200, data:data})
-        }
-        const collection = dataBase.collection("Users")
+        const collection = dataBase.collection("Zones")
         const data = await collection.aggregate([
             {
+                $group: {
+                    _id: '$Area',
+                },
+            },
+            {
                 $project: {
-                  _id:0,
-                  Password:0
-                }
-            }
+                  _id: 0,
+                  Area: '$_id',
+                },
+              },
         ]).toArray()
         res.status(200).send({status:200, data:data})
        
@@ -48,7 +31,7 @@ appUser.get("/", async(req,res)=>{
 
 //Crear un buscador para los usuarios
 //http://127.17.0.96:5099/users/SearchGeneral?text=""
-appUser.get("/SearchGeneral", async(req,res)=>{
+appZones.get("/SearchGeneral", async(req,res)=>{
     try {
         const {text} = req.query;
         const collection = dataBase.collection("Users")
@@ -81,7 +64,7 @@ appUser.get("/SearchGeneral", async(req,res)=>{
 
 //Crear un buscador para los usuarios
 //http://127.17.0.96:5099/users/Search?rol=Camper&text=
-appUser.get("/Search", async(req,res)=>{
+appZones.get("/Search", async(req,res)=>{
     try {
         const {rol, text} = req.query;
         if (rol == "Admin") {
@@ -112,4 +95,4 @@ appUser.get("/Search", async(req,res)=>{
 })
 
 
-export default appUser
+export default appZones

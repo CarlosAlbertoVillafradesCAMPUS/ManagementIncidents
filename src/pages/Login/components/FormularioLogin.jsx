7d1182d8b55de3b35ce967c08cbd745d595cd2a8
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-09-29 20:41:46 -0500

    feat: :sparkles: Creation of a Register
    The first style of the registration page was created
    
    by: Carlos Villafrades

--- src/pages/Login/components/FormularioLogin.jsx
+++ src/pages/Login/components/FormularioLogin.jsx
@@ -7,7 +7,7 @@ export default function FormularioLogin() {
     return (
         <div>
             <Titulos name="Iniciar Sesion" styles="Login" />
-            <div className='w-100 d-flex justify-content-center'>
+            <div className='w-100 d-flex justify-content-center mt-4'>
                 <form className='text-white mt-3 w-75 fs-5 formularioLogin'>
                     <div className="mb-3">
                         <label className="form-label">Email address:</label>
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home/Home'
 import Login from '../pages/Login/Login'
+import Signup from '../pages/Signup/Signup'
 import Camper from '../pages/Camper/Camper'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/signUp' element={<Signup />} />
         <Route path='/camper' element={<Camper />} />
       </Routes>
     </BrowserRouter>

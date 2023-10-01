f1c988f7caddd17104ec097bc1c1ca7aa8476f09
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-10-01 17:24:12 -0500

    feat: :sparkles: Creation of routes for each of our pages
    We created routes for each of our pages, and we also created a component that lets us privatize our routes.
    
    by:Carlos Villafrades

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home/Home'
 import Login from '../pages/Login/Login'
@@ -6,18 +6,48 @@ import Signup from '../pages/Signup/Signup'
 import Camper from '../pages/Camper/Camper'
 import Trainer from '../pages/Trainer/Trainer'
 import Support from '../pages/Support/Support'
+import ProtectedRoute from '../utils/ProtectedRoute'
+import { useLocation } from 'react-use'
+
+
 
 function App() {
+
+   const [activateLogin, setActivateLogin] = useState(true)
+  const [activatePages, setActivatePages] = useState(false)
+  const locationRoute = useLocation()
+
+  const activate = () =>{
+    const infoLocalStorage = localStorage.getItem("token")
+    if (infoLocalStorage) {
+      setActivateLogin(false)
+      setActivatePages(true)
+    }else{
+      setActivateLogin(true)
+      setActivatePages(false)
+    }
+  }
+
+  useEffect(() => {
+    activate();
+  }, [locationRoute]);
 
   return (
     <BrowserRouter>
       <Routes>
+      <Route element={<ProtectedRoute canActivate={activateLogin} redirectPath="/camper" />}>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signUp' element={<Signup />} />
+      </Route>
+      <Route element={<ProtectedRoute canActivate={activatePages} redirectPath="/" />}>
         <Route path='/camper' element={<Camper />} />
         <Route path='/trainer' element={<Trainer />} />
         <Route path='/support' element={<Support />} />
+      </Route>
+      <Route element={<ProtectedRoute canActivate={false} redirectPath="/" />}>
+        <Route path="*" element={<Home />} />
+      </Route>
       </Routes>
     </BrowserRouter>
   )
